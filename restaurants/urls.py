a647246812b3eeb56080ce384b02c5d1893dc736
enrique_bits <enrique_empo@hotmail.com>
2020-08-28 18:19:25 -0500

    Add crud api and statistics api endpoints

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -2,9 +2,17 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from restaurants import views
 
-urlpatterns = [
-    path('restaurants/', views.RestaurantList.as_view()),
-    path('restaurants/<str:pk>/', views.RestaurantDetail.as_view()),
-]
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('restaurants/',
+        views.RestaurantList.as_view(),
+        name='restaurant-list'),
+    path('restaurants/statistics/',
+        views.RestaurantStatistics.as_view(),
+        name='restaurant-statistics'),
+    path('restaurants/<str:pk>/',
+        views.RestaurantDetail.as_view(),
+        name='restaurant-detail'),
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,22 @@
+import math
 from restaurants.models import Restaurant
 from restaurants.serializers import RestaurantSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from django.contrib.gis.measure import Distance
+from django.contrib.gis.geos import Point
+from django.db.models import Avg, StdDev
 
+
+
+@api_view(['GET'])
+def api_root(request):
+    return Response({
+        'restaurants': reverse('restaurant-list', request=request)
+    })
 
 class RestaurantList(generics.ListCreateAPIView):
     queryset = Restaurant.objects.all()
@@ -11,3 +26,24 @@ class RestaurantList(generics.ListCreateAPIView):
 class RestaurantDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Restaurant.objects.all()
     serializer_class = RestaurantSerializer
+
+
+class RestaurantStatistics(APIView):
+    """
+    List of restaurants that fall inside the circle with center [x,y] y radius z
+    """
+
+    def get(self, request):
+        restaurants = Restaurant.objects.filter(
+            location__distance_lt=(
+                Point(float(request.query_params.get("longitude")),
+                      float(request.query_params.get("latitude"))),
+                Distance(m=request.query_params.get("radius"))))
+        count = len(restaurants)
+        agg = restaurants.aggregate(avg=Avg('rating'), std=StdDev('rating'))
+
+        return Response({
+            "count": count,
+            "avg": math.floor(agg["avg"]),
+            "std": agg["std"]
+        })
