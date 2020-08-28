36989f86e1834763a26d8d73bfbc44770a1434b2
enrique_bits <enrique_empo@hotmail.com>
2020-08-28 09:12:53 -0500

    Add csv and database migration

---
+++ restaurants/management/commands/load_data.py
@@ -0,0 +1,34 @@
+import csv
+from restaurants.models import Restaurant
+from django.core.management.base import BaseCommand
+
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str)
+
+
+    def handle(self, *args, **options):
+        with open(options['path']) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                _, created = Restaurant.objects.get_or_create(
+                    id=row['id'],
+                    rating=row['rating'],
+                    name=row['name'],
+                    site=row['site'],
+                    email=row['email'],
+                    phone=row['phone'],
+                    street=row['street'],
+                    city=row['city'],
+                    state=row['state'],
+                    lat=row['lat'],
+                    lng=row['lng']
+                )
+                # creates a tuple of the new object or
+                # current object and a boolean of if it was created
+
+            self.stdout.write(self.style.SUCCESS('Successfully write data into model'))
