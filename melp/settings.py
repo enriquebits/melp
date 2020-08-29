36d5b22adf823b3cf5ebb805690b2955ddae12d5
enrique_bits <enrique_empo@hotmail.com>
2020-08-28 19:39:59 -0500

    Edit geoDjango env variables

--- melp/settings.py
+++ melp/settings.py
@@ -156,6 +156,5 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
-
-GEOS_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgeos_c.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = '/app/.heroku/vendor/lib/libgdal.so' if os.environ.get('ENV') == 'HEROKU' else os.getenv('GDAL_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
