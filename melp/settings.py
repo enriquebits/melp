d74c096762dff5c5524d81065c85c8682717d600
enrique_bits <enrique_empo@hotmail.com>
2020-08-28 20:10:54 -0500

    Edit database engine

--- melp/settings.py
+++ melp/settings.py
@@ -156,5 +156,7 @@ DATABASES = {
         default=config('DATABASE_URL')
     )
 }
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
