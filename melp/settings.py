1fc8fad3482191736967345d1e40a249b045c679
enrique_bits <enrique_empo@hotmail.com>
2020-08-28 19:11:16 -0500

    Add decouple config module

--- melp/settings.py
+++ melp/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-from pathlib import Path
 import dj_database_url
+from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
