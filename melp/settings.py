221a902042a664fa13dbb3cb7060a68180e6e988
enrique_bits <enrique_empo@hotmail.com>
2020-08-28 19:46:46 -0500

    Edit whitenoise import

--- melp/settings.py
+++ melp/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
